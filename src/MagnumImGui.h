1cb1dbbfe776015178a7ff306da6a6166763df2b
Georg <gsperl@ldchwo009.ist.local>
2018-04-10 16:43:50 +0200

    lib include

--- src/MagnumImGui.h
+++ src/MagnumImGui.h
@@ -23,17 +23,16 @@
 #ifndef MagnumImgui_h__
 #define MagnumImgui_h__
 
-#include <Magnum/Platform/Sdl2Application.h>
 #include <Magnum/AbstractShaderProgram.h>
-#include <Magnum/Math/Matrix4.h>
-#include <Magnum/Texture.h>
-#include <Magnum/Buffer.h>
-#include <Magnum/Mesh.h>
-#include <Magnum/Timeline.h>
 #include <Magnum/Attribute.h>
+#include <Magnum/Buffer.h>
+#include <Magnum/Math/Matrix4.h>
+#include <Magnum/Mesh.h>
+#include <Magnum/Platform/Sdl2Application.h>
+#include <Magnum/Texture.h>
+#include <Magnum/Timeline.h>
 
-class ImguiShader : public Magnum::AbstractShaderProgram
-{
+class ImguiShader : public Magnum::AbstractShaderProgram {
 public:
   typedef Magnum::Attribute<0, Magnum::Vector2> Position;
   typedef Magnum::Attribute<1, Magnum::Vector2> TextureCoordinates;
@@ -41,14 +40,12 @@ public:
 
   ImguiShader();
 
-  ImguiShader& setProjectMatrix(const Magnum::Matrix4& matrix)
-  {
+  ImguiShader &setProjectMatrix(const Magnum::Matrix4 &matrix) {
     setUniform(mProjMatrixUniform, matrix);
     return *this;
   }
 
-  ImguiShader& setTexture(Magnum::Texture2D& texture)
-  {
+  ImguiShader &setTexture(Magnum::Texture2D &texture) {
     texture.bind(TextureLayer);
     return *this;
   }
@@ -59,30 +56,33 @@ private:
   Magnum::Int mProjMatrixUniform;
 };
 
-
-class MagnumImGui
-{
+class MagnumImGui {
 public:
   MagnumImGui();
   ~MagnumImGui();
 
-  void newFrame(const Magnum::Vector2i &winSize, const Magnum::Vector2i &viewportSize);
+  void newFrame(const Magnum::Vector2i &winSize,
+                const Magnum::Vector2i &viewportSize);
 
   void drawFrame();
 
-  bool mousePressEvent(const Magnum::Platform::Application::MouseEvent& event);
+  bool mousePressEvent(const Magnum::Platform::Application::MouseEvent &event);
 
-  bool mouseReleaseEvent(const Magnum::Platform::Application::MouseEvent& event);
+  bool
+  mouseReleaseEvent(const Magnum::Platform::Application::MouseEvent &event);
 
-  bool mouseScrollEvent(const Magnum::Platform::Application::MouseScrollEvent& event);
+  bool mouseScrollEvent(
+      const Magnum::Platform::Application::MouseScrollEvent &event);
 
-  bool mouseMoveEvent(const Magnum::Platform::Application::MouseMoveEvent& event);
+  bool
+  mouseMoveEvent(const Magnum::Platform::Application::MouseMoveEvent &event);
 
-  bool keyPressEvent(const Magnum::Platform::Application::KeyEvent& event);
+  bool keyPressEvent(const Magnum::Platform::Application::KeyEvent &event);
 
-  bool keyReleaseEvent(const Magnum::Platform::Application::KeyEvent& event);
+  bool keyReleaseEvent(const Magnum::Platform::Application::KeyEvent &event);
 
-  bool textInputEvent(const Magnum::Platform::Application::TextInputEvent& event);
+  bool
+  textInputEvent(const Magnum::Platform::Application::TextInputEvent &event);
 
 private:
   ImguiShader mShader;
@@ -92,7 +92,7 @@ private:
   Magnum::Timeline mTimeline;
   Magnum::Mesh mMesh;
 
-  bool mMousePressed[3] = { false, false, false };
+  bool mMousePressed[3] = {false, false, false};
   float mMouseScroll = 0.0f;
   Magnum::Vector2i mMousePos;
 
@@ -100,9 +100,12 @@ private:
   void init();
   void load();
 
-  bool keyPressReleaseEvent(const Magnum::Platform::Application::KeyEvent& event, bool value);
-  bool mousePressReleaseEvent(const Magnum::Platform::Application::MouseEvent& event, bool value);
+  bool
+  keyPressReleaseEvent(const Magnum::Platform::Application::KeyEvent &event,
+                       bool value);
+  bool
+  mousePressReleaseEvent(const Magnum::Platform::Application::MouseEvent &event,
+                         bool value);
 };
-
 
 #endif // MagnumImgui_h__
